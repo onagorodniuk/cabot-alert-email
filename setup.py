7df9f47389691dbe3f4c0e6910bab26118be15f3
onagorodniuk <gagarinq9@gmail.com>
2020-03-27 11:03:39 +0200

    Create own release with custom email_template based on original code base

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import find_packages, setup
 
 setup(name='cabot-alert-email',
-      version='1.4.3',
+      version='1.4.3.0',
       description='An email alert plugin for Cabot by Arachnys',
       author='Arachnys',
       author_email='info@arachnys.org',
       url='http://cabotapp.com',
       packages=find_packages(),
-      download_url= 'https://github.com/onagorodniuk/cabot-alert-email/archive/1.4.3.zip',
+      download_url= 'https://github.com/onagorodniuk/cabot-alert-email/archive/1.4.3.0.zip',
      )
